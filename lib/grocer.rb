948992b1803a6e95c7059f7312d796e71d18aa02
Eve Reichmann <ereichmann4@aol.com>
2020-06-17 17:30:23 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,11 +1,11 @@
-require 'pry'
+#require 'pry'
 def find_item_by_name_in_collection(name, collection)
   count = 0 
   collection.each do |item|
     while item[:item] == name
       return item
     end
-      count += 1
+    count += 1
   end
   nil
 end
@@ -21,25 +21,29 @@ end
 #end
 
 def consolidate_cart(cart)
-  counter = 0 
-  new_cart = Array.new
-  while counter < cart.length do 
-    new_cart_item = find_item_by_name_in_collection(cart[counter][:item], new_cart)
-    if new_cart_item
-      new_cart_item[:count] += 1 
-    else
-      new_cart_item = {
-        :item => cart[counter][:item],
-        :price => cart[counter][:price],
-        :clearance => cart[counter][:clearance],
-        :count => 1
-         }
-        new_cart << new_cart_item
-    end
-    counter += 1
-  end  
-  new_cart
+  new_cart = 
 end
+
+#def consolidate_cart(cart)
+#  counter = 0 
+#  new_cart = Array.new
+#  while counter < cart.length do 
+#    new_cart_item = find_item_by_name_in_collection(cart[counter][:item], new_cart)
+#    if new_cart_item
+#      new_cart_item[:count] += 1 
+#    else
+#      new_cart_item = {
+#        :item => cart[counter][:item],
+#        :price => cart[counter][:price],
+#        :clearance => cart[counter][:clearance],
+#        :count => 1
+#         }
+#        new_cart << new_cart_item
+#    end
+#    counter += 1
+#  end  
+#  new_cart
+#end
 
 # used video walkthrow tutural 
   
