576a5c45edf36b729aa057052786bdfc4bfcc227
Eve Reichmann <ereichmann4@aol.com>
2020-06-17 17:19:38 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,12 +1,19 @@
+require 'pry'
 def find_item_by_name_in_collection(name, collection)
-  counter = 0 
-  while counter < collection.length do
-    if collection[counter][:item] == name
-      return collection[counter] 
-    end
-    counter += 1 
-  end  
+  count = 0 
+  collection.each do |key, value|
+  binding.pry
+  end
 end
+#def find_item_by_name_in_collection(name, collection)
+#  counter = 0 
+#  while counter < collection.length do
+#    if collection[counter][:item] == name
+#      return collection[counter] 
+#    end
+#    counter += 1 
+#  end  
+#end
 
 def consolidate_cart(cart)
   counter = 0 
